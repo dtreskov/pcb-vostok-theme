69f98f0600b632b6844bba75f90f044a74de63aa
pcbvostok_admin <dtreskov95@gmail.com>
2026-09-14 17:46:32 +0300

    Merged changes from https://pcbvostok.tw1.ru on 09.14.2026

--- wp-content/themes/pcb-vostok/functions.php
+++ wp-content/themes/pcb-vostok/functions.php
@@ -266,3 +266,9 @@ add_action(
     'wp_enqueue_scripts',
     'theme_enqueue_scripts'
 );
+
+add_action( 'init', function () {
+    register_block_pattern_category( 'pcb-vostok', array(
+        'label' => __( 'PCB Восток', 'pcb-vostok' ),
+    ) );
+} );
